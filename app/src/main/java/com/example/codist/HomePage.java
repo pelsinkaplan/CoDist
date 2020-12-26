cf3026c9bd2715c6fd02b9e694fbf43fd6c9029a
pelsinkaplan <pelsinkaplan28@hotmail.com>
2020-12-26 22:31:42 +0300

    Bildirim ayarlamaları yapıldı.

--- app/src/main/java/com/example/codist/HomePage.java
+++ app/src/main/java/com/example/codist/HomePage.java
@@ -2,14 +2,21 @@ package com.example.codist;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -35,6 +42,7 @@ import java.util.Set;
 public class HomePage extends MainActivity {
     private static final String TAG = "bluetooth";
     BluetoothAdapter mBluetoothAdapter;
+    String body;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,70 +51,97 @@ public class HomePage extends MainActivity {
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
 
-
         // kullanıcı giriş yapmamışsa login sayfasına gönder
         if (FirebaseAuth.getInstance().getCurrentUser() == null) {
             changeActivity(MainActivity.getInstance().openLoginPage());
             finish();
         }
-
-
     }
 
     private BroadcastReceiver mBroadcastReceiver3 = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
-            Log.d(TAG,"onReceive: Action Found");
-            if(action.equals(BluetoothDevice.ACTION_FOUND)) {
+            Log.d(TAG, "onReceive: Action Found");
+            if (action.equals(BluetoothDevice.ACTION_FOUND)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                int  rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
-                double expr = (-69 - (double)rssi) / 20 ;
-                double meters = 2 * Math.pow(10,expr);
+                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
+                double expr = (-69 - (double) rssi) / 20;
+                double meters = 2 * Math.pow(10, expr);
                 Log.d(TAG, "onReceive: " + device.getName() + ": " + device.getAddress());
                 Log.d(TAG, "meters: " + meters + ": " + device.getName());
+                if (meters < 0.9) {
+                    body = "Sosyal Mesafe İhlali! (" + device.getName() + ")";
+                    notification();
+                }
             }
         }
     };
 
+    public void notification() {
+        NotificationManager notif = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notify = new Notification.Builder
+                (getApplicationContext()).setContentText(body).
+                setContentTitle("CoDist").setSmallIcon(R.drawable.common_google_signin_btn_icon_dark).build();
+
+        notify.flags |= Notification.FLAG_AUTO_CANCEL;
+        notif.notify(0, notify);
+
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+            Vibrator vi = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            // Vibrate for 500 milliseconds
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vi.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                //deprecated in API 26
+                vi.vibrate(500);
+            }
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void btnDiscover(View view) {
         //Checken ob Bluetooth an ist
         if (mBluetoothAdapter == null) {
-            Log.d(TAG,"enableDisableBT: Does not have Bluetooth capabilities");
+            Log.d(TAG, "enableDisableBT: Does not have Bluetooth capabilities");
         }
-        if(!(mBluetoothAdapter.isEnabled())) {
-            Log.d(TAG,"enableDisableBT: enabling BT.");
+        if (!(mBluetoothAdapter.isEnabled())) {
+            Log.d(TAG, "enableDisableBT: enabling BT.");
             Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivity(enableBTIntent);
         }
-        Log.d(TAG,"btnDiscover: Looking for unpaired Devices");
-        if(mBluetoothAdapter.isDiscovering()) {
+        Log.d(TAG, "btnDiscover: Looking for unpaired Devices");
+        if (mBluetoothAdapter.isDiscovering()) {
             mBluetoothAdapter.cancelDiscovery();
-            Log.d(TAG,"btnDiscover:Cancelling discovery.");
+            Log.d(TAG, "btnDiscover:Cancelling discovery.");
             checkBTPermissions();
             mBluetoothAdapter.startDiscovery();
             IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(mBroadcastReceiver3,discoverDevicesIntent);
+            registerReceiver(mBroadcastReceiver3, discoverDevicesIntent);
         }
 
-        if(!(mBluetoothAdapter.isDiscovering())) {
+        if (!(mBluetoothAdapter.isDiscovering())) {
             checkBTPermissions();
             mBluetoothAdapter.startDiscovery();
             IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(mBroadcastReceiver3,discoverDevicesIntent);
+            registerReceiver(mBroadcastReceiver3, discoverDevicesIntent);
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void checkBTPermissions() {
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
             int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
             permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
             if (permissionCheck != 0) {
 
                 this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
             }
-        }else{
+        } else {
             Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
         }
     }
